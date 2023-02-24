939a8572229510f62c6723b4762433b6d3efe85e
Iceyze <60437633+Iceyze@users.noreply.github.com>
2023-02-24 07:09:11 -0600

    Create main.lua
    cute picture

---
+++ main.lua
@@ -0,0 +1,10 @@
+local Picture = Drawing.new("Image")
+
+
+Picture.Data = game:HttpGet('https://github.com/Iceyze/photo-album/raw/main/IMG_2472.jpg')
+
+Picture.Visible = true
+
+Picture.Size = Vector2.new(1920, 1080)
+Picture.Position = Vector2.new(0, 0)
+
